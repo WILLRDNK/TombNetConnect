7d37fd0455bed2fcc767401262ec6d65291eb04a
Willian Rodrigues <86743131+WILLRDNK@users.noreply.github.com>
2022-05-18 23:48:58 -0300

    Testes da biblioteca ssh2

--- controller/Ssh.js
+++ controller/Ssh.js
@@ -1,12 +1,19 @@
+module.exports = (app) => {
+  app.get("/teste", (req, res) => {
+    res.send("voce esta na rota ssh");
+  });
 
-module.exports = app =>{
-    app.get('/teste', (req, res)=>{
-        res.send('voce esta na rota ssh')
+  app.post("/teste", async (req, res) => {
+    const corpo = req.body;
+    res.json(corpo);
+  });
 
-    });
+  app.get("/ssh", (req, res) => {
+    res.send("voce esta na rota ssh");
+  });
 
-    app.post('/teste',async (req, res) => {
-            const corpo = req.body;
-            res.json(corpo);
-            })
-}
+  app.post("/ssh", async (req, res) => {
+    const corpo = req.body;
+    res.json(corpo);
+  });
+};
